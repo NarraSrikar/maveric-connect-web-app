dd9e1250f2a96cf43c397c75fecfdbfdfff820f6
Narra Srikar Babu <narrasb@maveric-systems.com>
2022-03-27 15:26:02 +0530

    feature-login

---
+++ maveric-connect-webapp-feature-login/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Footer /> */}
+     <Header /> 
+      {/* <LoginPage /> */}
+     {/*<SignupPage />*/}
+    </div>
+  );
+}
+
+export default App;
